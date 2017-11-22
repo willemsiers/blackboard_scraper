bc60164438c7c6ac828fa6186a4fea9113cb4294
Willem Siers <willemhsiers@gmail.com>
2017-11-22 21:39:10 +0100

    adapt for BB university of twente

--- src/scrapergui.py
+++ src/scrapergui.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Tkinter import *
-import Tkinter
-import tkMessageBox
-import thread
+from tkinter import *
+import tkinter
+import tkinter.messagebox
+import _thread
 import webbrowser
-from tkFileDialog import askdirectory
+from tkinter.filedialog import askdirectory
 from bs4 import BeautifulSoup
 import os
 import requests
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import datetime
 import base64
 import string
@@ -18,7 +18,7 @@ import getpass
 import multiprocessing
 import functools
 from io import open as iopen
-from urlparse import urlsplit
+from urllib.parse import urlsplit
 import platform
 if "Darwin" in platform.system():
         os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
@@ -40,6 +40,7 @@ if "Darwin" in platform.system():
 
 
 valid_chars = '-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+BASE = "https://blackboard.utwente.nl"
 
 #sanitizes the filenames for windows (and hopefully other OS' too!)
 def sanitize(filename):
@@ -149,12 +150,13 @@ class BlackboardUnit():
     #folder name: the subfolder which the file will be downloaded to
     #path: the root path where the unit folder will be
     def fetch_document(self, file_url, folder_name, path):
+        print("fetching doc in ", folder_name, "("+file_url+")")
         while len(folder_name) > 50:
             folder_name = folder_name[:-1]
         while len(self.name) > 50:
             self.name = self.name[:-1]
-        folder_name = string.replace(folder_name, ':', ' ')
-        self.name = string.replace(self.name, ':', ' ')
+        folder_name = folder_name.replace(':', ' ')
+        self.name = self.name.replace( ':', ' ')
         folder_name = sanitize(folder_name)
         self.name = sanitize(self.name)
         urlResponse = self.session.get(file_url, allow_redirects=False)
@@ -170,7 +172,7 @@ class BlackboardUnit():
 
         name = urlsplit(urlpath)[2].split('/')
         name = name[len(name)-1]
-        name = urllib2.unquote(name).decode('utf8')
+        name = urllib.parse.unquote(name) #.decode('utf8')
         while ((len(thepath + name) > 240) or (len(name) > 50)):
             if "." in name:
                 filename = name.split('.')
@@ -187,12 +189,14 @@ class BlackboardUnit():
                 os.makedirs(thepath)
 
             if (not os.path.exists(thepath + name)):
-                print urlpath
+                if(urlpath[0] == '/') :
+                    urlpath = BASE + urlpath
                 urlResponse = self.session.get(urlpath)
                 if urlResponse.status_code == requests.codes.ok:
                     with iopen(thepath + name, 'wb') as file:
                         file.write(urlResponse.content)
                 else:
+                    print("Skipping file because already exists on disk")
                     return False
 
     #scrapes a page (and all pages on the page)
@@ -201,28 +205,32 @@ class BlackboardUnit():
     #visitlist: list of previously visited links - to avoid going around in circles
     #path: root path to save all files from the unit
     def recursiveScrape(self, content_id, folder_name, visitlist, path):
+        print("====> recurse at ", folder_name)
+
         url = \
-            'https://lms.curtin.edu.au/webapps/blackboard/content/listContent.jsp?course_id=_' \
+            BASE + '/webapps/blackboard/content/listContent.jsp?course_id=_' \
             + self.uid + '_1&content_id=_' + content_id + '_1&mode=reset'
         request = self.session.get(url)
         soup = BeautifulSoup(request.text, "html.parser")
         for htmlLink in soup.find_all('a'):
             link = htmlLink.get('href')
             if '.pdf' in link  or '.doc' in link or 'ppt' in link or 'xid' in link:
-                link = string.replace(link, 'https://lms.curtin.edu.au/', '')
-                link = string.replace(link, 'lms.curtin.edu.au/', '')
+                link = link.replace(BASE, '')
+                link = link.replace('blackboard.utwente.nl/', '')
                 name = htmlLink.text
                 if '1 slide per page' in name or '4 slides per page' in name:
                     name = urlsplit(link)[2].split('/')[-1] + '.pdf'
                 try:
-                    self.fetch_document('https://lms.curtin.edu.au/' + link, folder_name, path)
+                    if(link[0] == '/'):
+                        link = BASE +  link
+                    self.fetch_document(link, folder_name, path)
                 except:
-                    print "Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1]))
+                    print("Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1])))
 
         for htmlLink in soup.find_all('a'):
             link = htmlLink.get('href')
-            if link.startswith('https://lms.curtin.edu.au/webapps/blackboard/content/listContent.jsp?') or link.startswith('/webapps/blackboard/content/listContent.jsp?'):
-                link = link.replace('https://lms.curtin.edu.au/webapps/blackboard/content/listContent.jsp?course_id=_'
+            if link.startswith(BASE + '/webapps/blackboard/content/listContent.jsp?') or link.startswith('/webapps/blackboard/content/listContent.jsp?'):
+                link = link.replace(BASE + '/webapps/blackboard/content/listContent.jsp?course_id=_'
                                + self.uid + '_1&content_id=_', '')
                 link = link.replace('/webapps/blackboard/content/listContent.jsp?course_id=_'
                                + self.uid + '_1&content_id=_', '')
@@ -233,28 +241,29 @@ class BlackboardUnit():
                         visitlist.append(link)
                         visitlist = self.recursiveScrape(link, htmlLink.span.string, visitlist, path)
                 except:
-                    print "Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1]))
+                    print("Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1])))
         return visitlist
 
     #starts scraping the unit, path is where it will save the files to
     def startScrape(self, path):
+        print("==> startScrape")
         visitlist = []
-        request = self.session.get('https://lms.curtin.edu.au/webapps/blackboard/execute/launcher?type=Course&id=_' + self.uid + '_1')
+        request = self.session.get(BASE + '/webapps/blackboard/execute/launcher?type=Course&id=_' + self.uid + '_1')
         soup = BeautifulSoup(request.text, "html.parser")
         for htmlLink in soup.find_all('a'):
             link = htmlLink.get('href')
             if '.pdf' in link or 'xid' in link:
-                link.replace('https://lms.curtin.edu.au', '')
-                link.replace('lms.curtin.edu.au', '')
+                link.replace(BASE, '')
+                link.replace('blackboard.utwente.nl', '')
                 name = htmlLink.text.replace(' ', '')
                 if '1slideperpage' in name or '4slideperpage' in name:
                     name = urlsplit(w)[2].split('/')[-1] + '.pdf'  # tempfix for a particular computing unit
                 try:
-                    self.fetch_document('https://lms.curtin.edu.au/' + link, '', path)
+                    self.fetch_document(BASE + link, '', path)
                 except:
-                    print "Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1]))
-            elif link.startswith('https://lms.curtin.edu.au/webapps/blackboard/content/listContent.jsp?') or link.startswith('/webapps/blackboard/content/listContent.jsp?'):
-                link = link.replace('https://lms.curtin.edu.au/webapps/blackboard/content/listContent.jsp?course_id=_'
+                    print("Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1])))
+            elif link.startswith(BASE + '/webapps/blackboard/content/listContent.jsp?') or link.startswith('/webapps/blackboard/content/listContent.jsp?'):
+                link = link.replace(BASE + '/webapps/blackboard/content/listContent.jsp?course_id=_'
                                + self.uid + '_1&content_id=_', '')
                 link = link.replace('/webapps/blackboard/content/listContent.jsp?course_id=_'
                                + self.uid + '_1&content_id=_', '')
@@ -264,8 +273,8 @@ class BlackboardUnit():
                         visitlist.append(link)
                         visitlist = self.recursiveScrape(link, htmlLink.span.string, visitlist, path)
                 except:
-                    print "Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1]))
-        print self.name + ' has finished'
+                    print("Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1])))
+        print(self.name + ' has finished')
 
 #class for managing a blackboard session. Also holds lists of iLectures and units available to your session.
 class BlackboardSession():
@@ -273,39 +282,59 @@ class BlackboardSession():
         self.session = requests.Session()
         self.unitList = []
         self.iLectureList = []
-        self.password = base64.b64encode(password)
+        # self.password = base64.b64encode(password.encode("utf-8"))
+        self.password = password
         self.username = user
+        # self.payload = {
+        #     'login': 'Login',
+        #     'action': 'login',
+        #     'user_id': self.username,
+        #     'encoded_pw': self.password,
+        #     }
+
         self.payload = {
-            'login': 'Login',
-            'action': 'login',
-            'user_id': self.username,
-            'encoded_pw': self.password,
-            }
-        self.url = 'https://lms.curtin.edu.au/webapps/login/'
+        'action': 'login',
+        'login': 'Login',
+        'user_id': self.username,
+        'password': self.password
+        }
+        self.url = BASE + '/webapps/login/'
         self.session.post(self.url, data=self.payload)
         self.getUnitList()
         self.getILectureList()
 
     #gets all available units for current logged in user
     def getUnitList(self):
-        response = self.session.get('https://lms.curtin.edu.au/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_3_1')
-        soup = BeautifulSoup(response.text, "html.parser")
+
+        payload = {
+            'action' : 'refreshAjaxModule',
+            'modId' : '_4_1',
+            'tabId' : '_1_1',
+            'tab_tab_group_id' : '_1_1'
+        }
+
+        response = self.session.post(BASE + '/webapps/portal/execute/tabs/tabAction', data=payload);
+        # response = self.session.get(BASE + '/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1')
+
+        soup = BeautifulSoup(response.text, "lxml")
         for htmlLink in soup.find_all('a'):
             link = htmlLink.get('href')
-            if link.startswith(' /webapps/blackboard/execute/launcher?type=Course'
-                            ):
+
+            if 'launcher?type=Course' in link:
                 link = \
                     link.replace(' /webapps/blackboard/execute/launcher?type=Course&id=_'
                               , '')
                 link = link.replace('_1&url=', '')
                 self.unitList.append(BlackboardUnit(link, htmlLink.string.replace('/',''), self.session))
+        if(len(self.unitList) == 0) : 
+            print("No units found")
 
     #gets all available iLectures for current logged in user
     def getILectureList(self):
         try:
             for unit in self.unitList:
                 request = \
-                    self.session.get('https://lms.curtin.edu.au/webapps/blackboard/execute/launcher?type=Course&id=_'
+                    self.session.get(BASE + '/webapps/blackboard/execute/launcher?type=Course&id=_'
                            + unit.uid + '_1')
                 soup = BeautifulSoup(request.text, "html.parser")
                 for link in soup.find_all('a'):
@@ -314,7 +343,7 @@ class BlackboardSession():
                                         soup.find(id='courseMenu_link'
                                         ).get('title')[9:].replace('/','')))
         except:
-            print "Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1]))
+            print("Error: %s -  %s" % (sys.exc_info()[0], str(sys.exc_info()[1])))
 
 
 #start the GUI
@@ -341,7 +370,7 @@ class scrapergui(Frame):
         kw['width'] = 110
         self.blackboard_session = None
         self.path = '.'
-        apply(Frame.__init__, (self, Master), kw)
+        Frame.__init__(*(self, Master), **kw)
         self.__RootObj = Frame
         self.__Frame2 = Frame(self)
         self.__Frame2.pack(side='top', padx=5, pady=0)
@@ -428,13 +457,13 @@ class scrapergui(Frame):
     def __on_Button5_ButRel_1(self, Event=None):
         for lecs in map(int, self.__Listbox2.curselection()):
             url = self.blackboard_session.iLectureList[lecs].link
-            webbrowser.open('https://lms.curtin.edu.au' + url, new=1,
+            webbrowser.open(BASE + url, new=1,
                             autoraise=True)
 
     #scrape ilectures
     def __on_Button4_ButRel_1(self, Event=None):
         self.path = self.__Entry3.get()
-        thread.start_new_thread(ILectureUnit.scrape_ilectures, (self.__Entry4.get(), self.path))
+        _thread.start_new_thread(ILectureUnit.scrape_ilectures, (self.__Entry4.get(), self.path))
 
     #login and get unit list
     def __on_Button2_ButRel_1(self, Event=None):
@@ -457,7 +486,7 @@ class scrapergui(Frame):
         self.path = self.__Entry3.get()
         for unit in map(int, self.__Listbox1.curselection()):
             bbunit = self.blackboard_session.unitList[unit]
-            thread.start_new_thread(bbunit.startScrape, (self.path,))
+            _thread.start_new_thread(bbunit.startScrape, (self.path,))
 
 
 
